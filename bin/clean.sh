370a4e249c4247f84ffdb4abc3be0c117ee04f7d
Nick Stevens <nick@bitcurry.com>
2013-12-03 12:02:24 -0600

    Add clean shell script

---
+++ bin/clean.sh
@@ -0,0 +1,4 @@
+rm -rf ~/.vim
+rm -rf ~/.homesick/repos/vim-pathogen ~/.homesick/repos/vim-startify
+rm -rf ~/.homesick/repos/dotfiles/home/.vim/bundle/*
+rm -rf ~/.homesick/repos/dotfiles/home/.vim/autoload/*
