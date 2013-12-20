6631de77ef84e0cdca61defa7cd1587b499ff72f
Nick Stevens <nick@bitcurry.com>
2013-12-19 20:53:39 -0600

    Remove unneeded bootstrap files

--- bin/clean.sh
+++
@@ -1,4 +0,0 @@
-rm -rf ~/.vim
-rm -rf ~/.homesick/repos/vim-pathogen ~/.homesick/repos/vim-startify
-rm -rf ~/.homesick/repos/dotfiles/home/.vim/bundle/*
-rm -rf ~/.homesick/repos/dotfiles/home/.vim/autoload/*
