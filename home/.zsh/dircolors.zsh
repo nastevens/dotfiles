6b358ca975a9c4c4ff5edbdd36d769b0918443d1
Nick Stevens <nick@bitcurry.com>
2016-06-05 21:53:33 -0500

    Enable dircolors for OSX

--- home/.zsh/dircolors.zsh
+++ home/.zsh/dircolors.zsh
@@ -1,10 +1,24 @@
-# Enable colors for ls, etc.  Prefer ~/.dir_colors
-if whence -p dircolors >/dev/null ; then
-    if [[ -f ~/.dir_colors ]] ; then
-        eval $(dircolors -b ~/.dir_colors)
-    elif [[ -f /etc/DIR_COLORS ]] ; then
-        eval $(dircolors -b /etc/DIR_COLORS)
+# Find dircolors executable
+dircolors_exe=""
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    gls --color -d . &>/dev/null 2>&1 && alias ls='gls -FqN --color=auto' || alias ls='ls -GF'
+    gdircolors &> /dev/null 2>&1 && dircolors_exe=gdircolors
+else
+    ls --color -d . &>/dev/null 2>&1 && alias ls='ls -FqN --color=auto' || alias ls='ls -GF'
+    dircolors &> /dev/null 2>&1 && dircolors_exe=dircolors
+fi
+
+# Find config path, preferring ~/.dir_colors
+dircolors_path=""
+[ -f /etc/DIR_COLORS ] && dircolors_path="/etc/DIR_COLORS"
+[ -f $HOME/.dir_colors ] && dircolors_path="$HOME/.dir_colors"
+
+# Apply dircolors
+if [ -n "$dircolors_exe" ]; then
+    if [ -n "$dircolors_path" ]; then
+        eval $($dircolors_exe -b $dircolors_path)
     else
-        eval $(dircolors --print-database)
+        eval $($dircolors_exe --print-database)
     fi
 fi
+
