5cd334beb02d7263b1da12e1f4966ac94015123c
Nick Stevens <nick@bitcurry.com>
2022-02-01 18:35:03 -0600

    nvim: make default vim alias

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -5,7 +5,8 @@ alias grep="grep --colour=auto"
 alias rustfmt-nightly="rustup run nightly rustfmt --config-path ~/.nightly-rustfmt.toml --emit files --edition 2018"
 alias su="su -"
 alias tmux="tmux -2"
-alias vimupdate="vim +PlugUpgrade +PlugUpdate +qall"
+alias vim="nvim"
+alias vimupdate="nvim +PackerSync"
 alias x="tar axf"
 
 if hash gping >/dev/null 2>&1; then
