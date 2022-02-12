a9e45f858be8eff6ceaf61576a6d077102579665
Nick Stevens <nick@bitcurry.com>
2022-02-12 11:22:12 -0600

    nvim: improve cmp/luasnip key behavior

--- home/.config/nvim/lua/config/plugins/cmp.lua
+++ home/.config/nvim/lua/config/plugins/cmp.lua
@@ -1,5 +1,11 @@
 local M = {}
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 M.init = function()
   cmp.setup {
@@ -16,13 +22,44 @@ M.init = function()
       })
     },
     mapping = {
-      ['<C-p>'] = cmp.mapping.select_prev_item(),
-      ['<C-n>'] = cmp.mapping.select_next_item(),
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-n>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expandable() then
+          luasnip.expand_or_jump()
+        elseif luasnip.choice_active() then
+          luasnip.change_choice(1)
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, {"i", "s"}),
+      ['<C-p>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.choice_active() then
+          luasnip.change_choice(-1)
+        else
+          fallback()
+        end
+      end, {"i", "s"}),
+      ['<C-j>'] = cmp.mapping(function(fallback)
+        if luasnip.jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, {"i", "s"}),
+      ['<C-k>'] = cmp.mapping(function(fallback)
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, {"i", "s"}),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({
+      ['<Tab>'] = cmp.mapping.confirm({
         behavior = cmp.ConfirmBehavior.Insert,
         select = true,
       }),
