0260fca107688f39ff742be8ef65b85fb19c98f0
Nick Stevens <nick@bitcurry.com>
2022-02-28 14:52:40 -0600

    zsh: only alias vim=nvim if nvim available

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -5,9 +5,10 @@ alias grep="grep --colour=auto"
 alias rustfmt-nightly="rustup run nightly rustfmt --config-path ~/.nightly-rustfmt.toml --emit files --edition 2018"
 alias su="su -"
 alias tmux="tmux -2"
-alias vim="nvim"
 alias vimupdate="nvim +PackerSync"
 alias x="tar axf"
+
+command -v nvim >/dev/null 2>&1 && alias vim="nvim"
 
 if hash gping >/dev/null 2>&1; then
     alias ping=gping
