2f1296661b45dc489bedd895493a41e3e6214f76
Nick Stevens <nick@bitcurry.com>
2022-03-22 09:18:31 -0500

    nvim: nvim-tree config updates

--- home/.config/nvim/lua/config/plugins/nvim-tree.lua
+++ home/.config/nvim/lua/config/plugins/nvim-tree.lua
@@ -1,11 +1,17 @@
 local nvim_tree = require("nvim-tree")
 
 vim.g.nvim_tree_add_trailing = 1
-vim.g.nvim_tree_disable_window_picker = 1
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_group_empty = 1
 
 nvim_tree.setup {
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = true
+      },
+    },
+  },
   auto_close = true,
   diagonostics = {
     enable = true,
