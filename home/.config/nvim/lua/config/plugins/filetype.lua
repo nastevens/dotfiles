0a83e9d3fe83088513be88c483e6ded2d52747f4
Nick Stevens <nick@bitcurry.com>
2022-03-26 10:22:53 -0500

    nvim: add new filetypes

--- home/.config/nvim/lua/config/plugins/filetype.lua
+++ home/.config/nvim/lua/config/plugins/filetype.lua
@@ -1,14 +1,16 @@
 require("filetype").setup {
   overrides = {
     extensions = {
+      bb = "bitbake",
       tf = "terraform",
     },
     literal = {
       Jenkinsfile = "groovy",
     },
     complex = {
-      ["%.aws/config"] = "conf",
-      ["Dockerfile"] = "dockerfile",
+      [".*/%.aws/config"] = "conf",
+      [".*/Dockerfile[^/]*"] = "dockerfile",
+      [".*/hub%-os/.*%.inc"] = "bitbake",
     },
   }
 }
