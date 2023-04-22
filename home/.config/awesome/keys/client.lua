309ed0a2e5e2e31af3340e2a332a0bbb383d5e4b
Nick Stevens <nick@bitcurry.com>
2023-04-21 21:49:45 -0500

    awesome: keybinding rework

--- home/.config/awesome/keys/client.lua
+++ home/.config/awesome/keys/client.lua
@@ -3,44 +3,36 @@ local gears = require("gears")
 local add_key = require("keys.utils").add_key
 
 local function create()
-    return gears.table.join(
-        add_key("M-f", "toggle fullscreen", "client", function(c)
-            c.fullscreen = not c.fullscreen
-            c:raise()
-        end),
-        add_key("M-S-c", "close", "client", function(c)
-            c:kill()
-        end),
-        add_key("M-C-space", "toggle floating", "client", function(c)
-            c.floating = not c.floating
-        end),
-        add_key("M-C-Return", "move to master", "client", function(c)
-            c:swap(awful.client.getmaster())
-        end),
-        add_key("M-o", "move to screen", "client", function(c)
-            c:move_to_screen()
-        end),
-        add_key("M-t", "toggle keep on top", "client", function(c)
-            c.ontop = not c.ontop
-        end),
-        add_key("M-n", "minimize", "client", function(c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end),
-        add_key("M-m", "(un)maximize", "client", function(c)
-            c.maximized = not c.maximized
-            c:raise()
-        end),
-        add_key("M-C-m", "(un)maximize vertically", "client", function(c)
-            c.maximized_vertical = not c.maximized_vertical
-            c:raise()
-        end),
-        add_key("M-S-m", "(un)maximize horizontally", "client", function(c)
-            c.maximized_horizontal = not c.maximized_horizontal
-            c:raise()
-        end)
-    )
+	return gears.table.join(
+		add_key("M-f", "toggle fullscreen", "client", function(c)
+			c.fullscreen = not c.fullscreen
+			c:raise()
+		end),
+		add_key("M-S-c", "close", "client", function(c)
+			c:kill()
+		end),
+		add_key("M-C-space", "toggle floating", "client", function(c)
+			c.floating = not c.floating
+		end),
+		add_key("M-a", "move to master", "client", function(c)
+			c:swap(awful.client.getmaster())
+		end),
+		add_key("M-o", "move to screen", "client", function(c)
+			c:move_to_screen()
+		end),
+		add_key("M-m", "(un)maximize", "client", function(c)
+			c.maximized = not c.maximized
+			c:raise()
+		end),
+		add_key("M-C-m", "(un)maximize vertically", "client", function(c)
+			c.maximized_vertical = not c.maximized_vertical
+			c:raise()
+		end),
+		add_key("M-S-m", "(un)maximize horizontally", "client", function(c)
+			c.maximized_horizontal = not c.maximized_horizontal
+			c:raise()
+		end)
+	)
 end
 
 return setmetatable({}, { __call = create })
