578ec2fa7d08706a950ff4b4713a5af790814254
Nick Stevens <nick@bitcurry.com>
2024-02-26 17:02:40 -0600

    nvim: only add vimwiki bindings if available

--- home/.config/nvim/lua/config/plugins/vimwiki.lua
+++ home/.config/nvim/lua/config/plugins/vimwiki.lua
@@ -39,12 +39,12 @@ if vim.env.DROPBOX then
             autocmd BufRead scratchpad.wiki setfiletype vimwiki
         augroup END
     ]])
-end
 
--- Vimwiki mappings
-vimp.nmap({ "override" }, "<leader>wp", "<cmd>norm 1<leader>ww<cr>")
-vimp.nmap({ "override" }, "<leader>wo", "<cmd>norm 2<leader>ww<cr>")
-vimp.nnoremap("<leader>sc", function()
-    local path = vim.env.DROPBOX .. "/scratchpad.wiki"
-    util.smart_open_file(path)
-end)
+    -- Vimwiki mappings
+    vimp.nmap({ "override" }, "<leader>wp", "<cmd>norm 1<leader>ww<cr>")
+    vimp.nmap({ "override" }, "<leader>wo", "<cmd>norm 2<leader>ww<cr>")
+    vimp.nnoremap("<leader>sc", function()
+        local path = vim.env.DROPBOX .. "/scratchpad.wiki"
+        require("config.util").smart_open_file(path)
+    end)
+end
