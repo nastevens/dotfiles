e1a04c3845f42207a24f2cf59e9b04b95cb51c86
Nick Stevens <nick@bitcurry.com>
2024-02-29 14:59:50 -0600

    zsh: add exa aliases with backup ls aliases

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -17,16 +17,25 @@ alias_if_exists bandwhich bandwhich "sudo $HOME/.cargo/bin/bandwhich"
 alias_if_exists bc bc "bc -l"
 alias_if_exists garden ga garden
 alias_if_exists gping ping gping
-alias_if_exists exa l exa
-alias_if_exists exa ll "exa -lh"
-alias_if_exists exa la "exa -a"
-alias_if_exists exa lla "exa -la"
 alias_if_exists nvim vim nvim
 alias_if_exists nvim vimupdate "nvim +PackerSync"
 alias_if_exists rustup rustfmt-nightly "rustup run nightly rustfmt --config-path ~/.nightly-rustfmt.toml --emit files --edition 2021"
 alias_if_exists smartthings st smartthings
 alias_if_exists tmux tmux "tmux -2"
 alias_if_exists xdg-open open xdg-open
+
+if command -v exa >/dev/null 2>&1; then
+    alias l=exa
+    alias ls=exa
+    alias ll="exa -lh"
+    alias la="exa -a"
+    alias lla="exa -la"
+else
+    alias l=ls
+    alias ll="ls -lh"
+    alias la="ls -a"
+    alias lla="ls -la"
+fi
 
 # Colorize help output
 alias_if_exists bat bathelp "bat --plain --language=help"
